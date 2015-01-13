c35728963d8c0d4751b865d5996094692b8f58f0
Kartik Cating-Subramanian <ksubramanian@chef.io>
2015-01-13 19:56:59 +0000

    Copied over previous motd file.

--- cookbooks/motd/recipes/default.rb
+++ cookbooks/motd/recipes/default.rb
@@ -3,3 +3,10 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+file "/etc/motd" do
+  action :create
+  content "Property of Chef"
+  mode "0644"
+  owner "root"
+  group "root"
+end
