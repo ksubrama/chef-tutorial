d73b23099c6397884cf733467915597c773e675f
Kartik Cating-Subramanian <ksubramanian@chef.io>
2015-01-14 18:29:31 +0000

    Add some apache tests.

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,3 +3,10 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+package 'httpd' do
+  action :install
+end
+
+service 'httpd' do
+  action [:start, :enable]
+end
--- cookbooks/apache/test/integration/default/serverspec/default_spec.rb
+++ cookbooks/apache/test/integration/default/serverspec/default_spec.rb
@@ -2,7 +2,21 @@ require 'serverspec'
 set :backend, :exec
 
 describe 'apache' do
-  it "is installed" do
-    expect(package("httpd")).to be_installed
+  describe package('httpd') do
+    it { should be_installed }
   end
+
+  describe service('httpd') do
+    it { should be_running }
+    it { should be_enabled }
+  end
+
+  describe port(80) do
+    it { should be_listening.on('127.0.0.1').with('tcp') }
+  end
+
+  describe command('curl http://localhost/') do
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should match /hello/ }
+  end 
 end
