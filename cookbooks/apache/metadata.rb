fc682415ed6c571e13563db49f0cd7889c69a879
Kartik Null Cating-Subramanian <ksubramanian@chef.io>
2015-01-15 15:24:33 -0500

    Bump version numbers for the apache version.  Make the server display fqdn.

--- cookbooks/apache/metadata.rb
+++ cookbooks/apache/metadata.rb
@@ -1,8 +1,8 @@
 name             'apache'
-maintainer       'The Authors'
-maintainer_email 'you@example.com'
+maintainer       'Kartik Cating-Subramanian'
+maintainer_email 'ksubramanian@chef.io'
 license          'all_rights'
 description      'Installs/Configures apache'
 long_description 'Installs/Configures apache'
-version          '0.1.0'
+version          '0.2.0'
 
